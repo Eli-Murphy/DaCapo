772410d281268cbc0565f952c3f2c3a944622ff6
ILikeBrunch <emurphy24@gcds.net>
2023-01-06 15:21:29 -0500

    Update DashCytoTest2.py

--- DashCytoTest2.py
+++ DashCytoTest2.py
@@ -1,16 +1,32 @@
-import json
-from dash import Dash, html, dcc, Input, Output
+from dash import Dash, html, Input, Output
 import dash_cytoscape as cyto
 import os
-from num2words import num2words
 
 cyto.load_extra_layouts()
 
-file = r"C:\Users\elija\Documents\GitHub\DaCapo\PARK17.csv"
+for x in os.listdir(r"C:\Users\elija\Documents\GitHub\DaCapo"):
+    if x.endswith(".csv"):
+        print(x)
+
+fdir = r"C:\Users\elija\Documents\GitHub\DaCapo"
+fname = "PARK17.csv"
+
+file = str(os.path.join(fdir, fname))
+
+file2 = r"C:\Users\elija\Documents\GitHub\DaCapo\PARK17.csv"
+
+print(file)
 
 protienData = []
 
+ncount = 0
+ecount = 0
+weightAdd = 0
+
+#csv = open(file, "r")
+
 with open(file, "r") as csv:
+
     next(csv)
     data = csv.readlines()
     for r in range(len(data)):
@@ -19,7 +35,8 @@ with open(file, "r") as csv:
         if str(row[0]) in str(protienData):
             continue
         else:
-            temp['data'] = {'id': row[0], 'label': row[0]}
+            temp['data'] = {'id': row[0], 'label': row[0], 'sourceCount': 1, 'targetCount': 1}
+            ncount += 1
         protienData.append(temp)
 
 
@@ -28,8 +45,10 @@ with open(file, "r") as csv:
         row = data[e].split(",")
         temp['data'] = {'source': row[0], 'target': row[1], 'weight': row[12]}
         protienData.append(temp)
+        ecount += 1
+        weightAdd += float(row[12])
 
-
+    aveWeight = round((weightAdd / ecount), 4)
     #for i in range(len(protienData)):
         #print(protienData[i])
 
@@ -39,9 +58,10 @@ styles = {
     'container': {
         'position': 'fixed',
         'display': 'flex',
-        'flex-direction': 'column',
+        'flexDirection': 'column',
         'height': '100%',
-        'width': '100%'
+        'width': '100%',
+        'font-family': 'Arial, Helvetica, sans-serif'
     },
     'cy-container': {
         'flex': '1',
@@ -52,6 +72,18 @@ styles = {
         'width': '100%',
         'height': '100%',
         'z-index': 999
+    },
+    'NumNodeEdge': {
+        'position': 'relative',
+        'display': 'flex',
+        'flexDirection': 'column',
+        'height': '100%',
+        'width': '100%',
+        'left': '500px',
+        'top':'-50px',
+        'font-size' : '16px',
+        'font-weight': 'normal',
+        'font-family': 'Arial, Helvetica, sans-serif'
     }
 }
 
@@ -60,14 +92,18 @@ app = Dash(__name__)
 
 app.layout = html.Div(style=styles['container'], children=[
     html.Div([
-        html.Button("Responsive Toggle", id='toggle-button'),
-            html.Div(id='toggle-text')
+        html.Button("Responsive", id='toggle-button'),
+            html.Div(id='toggle-text'),
+                ]),
+    html.Div([
+        html.H3("# of Nodes: {n}  |||  # of Edges: {e}  ||| Average Edge Weight: {a}".format(n = ncount, e = ecount, a = aveWeight), style=styles['NumNodeEdge'])
     ]),
     html.Div(className='cy-container', style=styles['cy-container'], children=[
         cyto.Cytoscape(
-            id='PARK17',
+            id='protienNetwork',
             layout={
                     'name': 'dagre',
+                    'spacingFactor': 3,
                     'idealEdgeLength': 100,
                     'nodeOverlap': 20,
                     'refresh': 20,
@@ -78,7 +114,7 @@ app.layout = html.Div(style=styles['container'], children=[
                     'nodeRepulsion': 400000,
                     'edgeElasticity': 100,
                     'nestingFactor': 5,
-                    'gravity': 80,
+                    'gravity': 0,
                     'numIter': 1000,
                     'initialTemp': 200,
                     'coolingFactor': 0.95,
@@ -100,6 +136,42 @@ app.layout = html.Div(style=styles['container'], children=[
                         'label': 'data(weight)'
                     }
                 },
+
+                #LINE COLORS START
+
+                {
+                    'selector': '[weight >= 0][weight <= .2]',
+                    'style': {
+                        'line-color': '#C5E8B7'
+                    }
+                },
+                {
+                    'selector': '[weight > .2 ][weight <= .4]',
+                    'style': {
+                        'line-color': '#ABE098'
+                    }
+                },
+                {
+                    'selector': '[weight > .4 ][weight <= .6]',
+                    'style': {
+                        'line-color': '#83D475'
+                    }
+                },
+                {
+                    'selector': '[weight > .6 ][weight <= .8]',
+                    'style': {
+                        'line-color': '#57C84D'
+                    }
+                },
+                {
+                    'selector': '[weight > .8 ][weight <= 1]',
+                    'style': {
+                        'line-color': '#2EB62C'
+                    }
+                },
+
+                #LINE COLOR END
+
                 {
                     'selector': 'edge',
                     'style': {
@@ -110,26 +182,34 @@ app.layout = html.Div(style=styles['container'], children=[
                 {
                     'selector': 'edge',
                     'style': {
-                        'source-arrow-color': 'black',
+                        'source-arrow-color': 'gray',
                         'source-arrow-shape': 'triangle',
-                        'line-color': 'gray'
+                        #'line-color': 'gray'
                     }
                 }
             ]
         ),
-            html.P(id='cytoscape-tapEdgeData-output')
+            html.P(id='cytoscape-tapEdgeData-output'),
+            html.P(id='cytoscape-tapNodeData-output')
     ])
 ])
 
 
 @app.callback(Output('cytoscape-tapEdgeData-output', 'children'),
-              Input('PARK17', 'tapEdgeData'))
+              Input('protienNetwork', 'tapEdgeData'))
 def displayTapEdgeData(data):
     if data:
         return "EDGE SELECT:   ", "SOURCE: ", data['source'], "  |  TARGET: ", data['target'], '  |  WEIGHT: ', data['weight']
 
 
-@app.callback(Output('PARK17', 'responsive'), 
+@app.callback(Output('cytoscape-tapNodeData-output', 'children'),
+              Input('protienNetwork', 'tapNodeData'))
+def displayTapEdgeData(data):
+    if data:
+        return "NODE SELECT:   ", "NAME: ", data['id']
+
+
+@app.callback(Output('protienNetwork', 'responsive'), 
                 [Input('toggle-button', 'n_clicks')])
 
 def toggle_responsive(n_clicks):
@@ -138,7 +218,7 @@ def toggle_responsive(n_clicks):
     return toggle_on
 
 
-@app.callback(Output('toggle-text', 'children'), [Input('PARK17', 'responsive')])
+@app.callback(Output('toggle-text', 'children'), [Input('protienNetwork', 'responsive')])
 def update_toggle_text(responsive):
     return '\t' + 'Responsive ' + ('On' if responsive else 'Off')
 
